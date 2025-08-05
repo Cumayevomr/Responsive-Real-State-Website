6e2bae558129f1e351717ab9bfcd5bae73d2806a
Cumayevomr <cumayevomr8@gmail.com>
2025-08-05 16:01:26 +0400

    show-scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,19 +58,24 @@ function scrollActive(){
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
         sectionTop = current.offsetTop - 58,
-        sectioId = current.getAttribute('id')
+        sectionId = current.getAttribute('id')
 
     if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-        document.querySelector('.nav__menu a[href*=' + sectioId + ']').classList.add('active-link')
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
     } else {
-        documentq.querySelector('.nav__menu a[href*=' + sectioId + ']').classList.remove('active-link')
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
     }
     })
 }
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById("scroll-up");
 
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
